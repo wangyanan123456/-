90f8a67a150738ba14f8ed7ee3fd9c9534d35270
wangyanan <>
2017-12-27 14:14:19 +0800

    商品管理

---
+++ js/goodsManage.js
@@ -0,0 +1,51 @@
+
+  $(document).ready(function(){
+    document.getElementById('checkItems').onclick=function(){  
+      // $('firstBilXuan input').hasClass("on_check")? $('firstBilXuan input').removeClass("on_check"):$('firstBilXuan input').addClass("on_check");
+       // 获取所有的复选框  
+          var checkElements=document.getElementsByName('items');  
+          if (this.checked) {  
+              for(var i=0;i<checkElements.length;i++){  
+                  var checkElement=checkElements[i];  
+                  checkElement.checked="checked";  
+              }  
+
+          }  
+          else{  
+              for(var i=0;i<checkElements.length;i++){  
+                  var checkElement=checkElements[i];  
+                  checkElement.checked=null;  
+              }  
+          }  
+
+    }  
+
+
+    $('.firstUlLeftLi').click(function(){
+      $(this).next('div').toggleClass('apper')
+    })
+     // 二级标题
+
+    $('.twoCateTitle').click(function(){
+     $(this).next('div').toggleClass('apper')
+      
+    })
+    // 三级标题
+
+    $('.threeCate div').each(function(index,lel){
+      
+      $(this).click(function(){
+        $('.threeCate div').css({'background':'#fff','border':'none'})
+        $(this).css({'background':'#EFF4FA','border-left':'1px solid #5B8CFF'})
+      })
+    })
+    // 时间弹窗
+    $('.buton').click(function(){
+        console.log(88)
+          $('.newCate').css('display','none');
+          $('.zhezhao').css('display','none');
+    })
+
+    
+       
+  })
